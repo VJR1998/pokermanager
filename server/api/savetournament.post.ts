15041adad6756b8f0af1315f3d2fc3e7b8ef315d
Vince <vincent.j.rankine@gmail.com>
2025-05-04 19:38:06 +0100

    Updated api, added savetournament post route

---
+++ server/api/savetournament.post.ts
@@ -0,0 +1,27 @@
+import { Pool } from 'pg'
+
+const config = useRuntimeConfig()
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+
+  console.log("SERVER", body);
+
+  const client = await pool.connect()
+  try {
+    const res = await client.query(
+      'UPDATE tournaments SET title = $1::varchar, description = $2::varchar, starting_stack = $3::bigint, chipset = $4::jsonb, blinds = $5::jsonb, money = $6::jsonb, seating = $7::jsonb WHERE id = $8::uuid;',
+      [body.title, body.description, body.starting_stack, JSON.stringify(body.chipset), JSON.stringify(body.blinds), JSON.stringify(body.money), JSON.stringify(body.seating), body.id]
+    )
+    return "Tournament Updated";
+  } catch (err) {
+    console.error('Server error:', err)
+    return createError({ statusCode: 500, statusMessage: 'Server error: ' + err })
+  } finally {
+    client.release()
+  }
+})
---
+++ server/api/tournament.get.ts
@@ -0,0 +1,17 @@
+import { Pool } from 'pg'
+
+const config = useRuntimeConfig()
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+export default defineEventHandler(async () => {
+  const client = await pool.connect()
+  try {
+    const res = await client.query('SELECT * FROM tournaments;')
+    return res.rows[0]
+  } finally {
+    client.release()
+  }
+})
