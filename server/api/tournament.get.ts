4e7adde39c08d6fd96588767aa96249983f77875
Vince <vincent.j.rankine@gmail.com>
2025-05-07 09:39:23 +0100

    Added user account check for tournament api endpoint

--- server/api/tournament.get.ts
+++ server/api/tournament.get.ts
@@ -7,14 +7,13 @@ const pool = new Pool({
 })
 
 export default defineEventHandler(async (event) => {
-  const { user } = await requireUserSession(event)
+  const { user } = await requireUserSession(event);
 
   const client = await pool.connect()
   try {
-    const res = await client.query('SELECT * FROM tournaments;');
-    // const res = await client.query('SELECT * FROM tournaments t JOIN users u ON u.userid = t.owner WHERE u.email = $1;',
-    //   [user.email]
-    // )
+    const res = await client.query('SELECT * FROM tournaments t JOIN users u ON u.id = t.owner WHERE u.email = $1;',
+      [user.email]
+    )
     return res.rows[0];
   } finally {
     client.release()
